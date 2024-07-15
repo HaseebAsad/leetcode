33cdad8b28038314afea118f9c6a3611802b7f89
Haseeb Asad <89456132+HaseebAsad@users.noreply.github.com>
2024-07-15 14:13:16 +0100

    Update 1701. Average Waiting Time.py
    unpack index in for loop to avoid memory errors in leetcode server

--- 1701. Average Waiting Time.py
+++ 1701. Average Waiting Time.py
@@ -1,7 +1,6 @@
 class Solution(object):
-    
-    def __init__(self):
 
+    def __init__(self):
         self.currentTime = 0
         self.totalWaitTime = 0
 
@@ -10,14 +9,10 @@ class Solution(object):
         :type customers: List[List[int]]
         :rtype: float
         """
-        # customers = sorted(customers, key=lambda x: (x[0]))
-        self.currentTime = self.currentTime = customers[0][0]
-        for i in range(0,len(customers)):
-            arrivalTime = customers[i][0]
-            if arrivalTime < self.currentTime:
-                waitTime = self.currentTime - arrivalTime # Time customer had to wait to be served
-                self.totalWaitTime += customers[i][1] + waitTime
-            else:
-                self.totalWaitTime += customers[i][1]
-            self.currentTime += customers[i][1]
-        return (self.totalWaitTime/len(customers))
+        self.currentTime = customers[0][0]
+        for arrivalTime, cookTime in customers:
+            if arrivalTime > self.currentTime:
+                self.currentTime = arrivalTime
+            self.totalWaitTime += self.currentTime + cookTime - arrivalTime
+            self.currentTime += cookTime
+        return self.totalWaitTime / len(customers)
